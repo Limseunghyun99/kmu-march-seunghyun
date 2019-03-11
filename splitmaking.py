52351d0d3a6c38f1dce270ae1bddfe977f0c0c9e
Seunghyun Lim <lcj3722@kookmin.ac.kr>
2019-03-11 13:20:02 +0900

    1st assignment

---
+++ splitmaking.py
@@ -0,0 +1,20 @@
+
+
+f = open('98429.txt', 'r')
+lines = f.readlines()
+dic = {}
+f.close()
+
+
+for line in lines:
+    line.rstrip('\n')
+    temp = []
+    temp = line.split(": ")
+    if temp[0] == "\n":
+        break
+    dic[temp[0]] = temp[1]
+
+
+print(dic)
+print(lines)
+
